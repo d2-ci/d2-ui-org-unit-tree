7a835df25abf624f58d83e962a33f357f8c32b74
Hendrik de Graaf <hendrik@dhis2.org>
2019-03-04 13:37:45 +0000

    5098ee1 v5.2.8

---
+++ styles/OUCheckbox.component.styles.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = {
+    checkbox: {
+        position: 'relative',
+        bottom: 2,
+        padding: 0
+    },
+    uncheckedCheckbox: {
+        fontSize: 15,
+        color: '#E0E0E0'
+    }
+};
